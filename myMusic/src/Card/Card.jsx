c5b96dda94f07b882835d73bf73d3552e104b788
Martin Olson <martinguyolson@gmail.com>
2023-03-06 18:32:33 +1100

    basic styling

--- myMusic/src/Card/Card.jsx
+++ myMusic/src/Card/Card.jsx
@@ -3,10 +3,12 @@ import styles from "./Card.module.scss";
 const Card = ({ title, band, cover, description }) => {
   return (
     <>
-      <h3>{title}</h3>
-      <p>{band}</p>
-      <p>{description}</p>
-      <img src={cover} className={styles.albumCover}></img>
+      <div className={styles.Card}>
+        <h3>{title}</h3>
+        <p>{band}</p>
+        <p>{description}</p>
+        <img src={cover} className={styles.albumCover}></img>
+      </div>
     </>
   );
 };
--- myMusic/src/CardHolder/CardHolder.jsx
+++ myMusic/src/CardHolder/CardHolder.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Card from "../Card/Card";
 import Albums from "../assets/Albums/Albums.json";
+import styles from "./CardHolder.module.scss";
 
 const CardHolder = () => {
   const albums = Object.values(Albums);
 
   return (
     <>
-      <div>
+      <div className={styles.CardHolder}>
         {albums[0].map((album, index) => {
           console.log(album.AlbumArtSrc);
           return (
