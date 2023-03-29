cd245a81c7c2f33bcd938737161fe62ace98aece
Martin Olson <martinguyolson@gmail.com>
2023-03-29 12:28:09 +1100

    through a combination of use state and multiple class names have tied a flip animation to the onclick

--- myMusic/src/Card/Card.jsx
+++ myMusic/src/Card/Card.jsx
@@ -5,6 +5,7 @@ import styles from "./Card.module.scss";
 
 const Card = ({ title, band, coverImgLink, description }) => {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [animateFlip, setAnimateFlip] = useState(false);
 
   return (
     //so design for this is image only, but it pops up into a modal with more information on click
@@ -12,8 +13,11 @@ const Card = ({ title, band, coverImgLink, description }) => {
       <div className={styles.Card}>
         <img
           src={coverImgLink}
-          className={styles.albumCover}
-          onClick={() => setModalOpen(!isModalOpen)}
+          className={animateFlip ? styles.albumCoverFlipped : styles.albumCover}
+          onClick={() => [
+            setModalOpen(!isModalOpen),
+            setAnimateFlip(!animateFlip),
+          ]}
         ></img>
         {isModalOpen ? (
           <AlbumDetails
